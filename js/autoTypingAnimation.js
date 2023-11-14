22b4fa4375ddfcd682641d69bb387f90264a3177
Ahmed-26 <ahmedrasheed6008@gmail.com>
2023-11-14 12:38:49 +0500

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $('.about-info-inner').typed({
+    strings: [
+      "<span>Hi, I am Ahmed Rasheed</span>  <br><br> Front-end Developer with 1 year of work experience building consumer-focused online products and services. Designed and developed web apps "
+    ],
+    typeSpeed: 7,
+    contentType: 'html'
+  });
+}, 100);
